0a90a2b6317d0fbdd5d96ef39671a0629cbaface
MPetrov99 <98600216+MPetrov99@users.noreply.github.com>
2022-05-04 02:32:12 +0300

    Build 1 - Styling and adjusting

---
+++ scripts.js
@@ -0,0 +1,48 @@
+// Feedback Slider
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 4,
+            loop: false
+        }
+    }
+})
+
+// Lightbox
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const selectedImages = document.querySelectorAll('.terminals_container img');
+selectedImages.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        lightbox.style.transition = 'background-color 400ms';
+
+        const img = document.createElement('img');
+        img.src = image.src;
+
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+
+        }
+        lightbox.appendChild(img);
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target != e.currentTarget) return
+    lightbox.classList.remove('active')
+})
